a28d80d971577f7face3368824c558a4cd7102a9
stefenism <stefenisms@gmail.com>
2018-08-11 01:56:49 -0500

    testing some preliminary stuff. -Stefen

---
+++ Assets/Scripts/MouseUtilities/MouseUtilities.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseUtilities : MonoBehaviour 
+{
+
+	static Vector2 mouseWorldPosition;
+
+}
---
+++ Assets/Scripts/MouseUtilities/testGridBlock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testGridBlock : MonoBehaviour {
+
+	SpriteRenderer sprite;
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnMouseOver()
+	{
+		sprite.color = Color.yellow;
+	}
+
+	void OnMouseExit()
+	{
+		sprite.color = Color.white;
+	}
+}
