709ce3a94563584d25d997a6bbed35a663b28a91
DESKTOP-6JC4FH9\Zack-PC <zack.gilbertson@gmail.com>
2018-08-12 19:58:41 -0500

    -Added Win and Lose Screen

---
+++ Assets/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	public void GoToGame() {
+		SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
